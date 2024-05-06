ae84efb596c7171f5810237b93e03afde180a21f
Thannia Hernandez <thaherna1@gmail.com>
2024-05-05 20:06:38 -0500

    finished writting function for index.js

--- index.js
+++ index.js
@@ -1,9 +1,95 @@
+//Imports inquirer package to be used in the file
 const inquirer = require('inquirer');
 
+//Calls file system
 const fs = require('fs');
 
-const questionPrompts = {
-    type: '',
-    name: '',
-    input:'',
+//Created an array of questions
+const questionPrompts = [
+    {
+        //Grabs the logo text from the command line prompt
+        type:'input',
+        message: 'Please enter logo text that is at least three characters long',
+        name: 'logoText',
+        //This will ensure input is only three characters
+        validate: function(input){
+            if (input.length > 3){
+                return 'Logo text must be only three characters.';
+            }
+            return true;
+        }
+    },
+    {
+        //Prompts the text color from the terminal input
+        type:'input',
+        message: 'Please enter logo text color value',
+        name: 'logoTextColor',
+    },
+    {
+        // Prompts the user what type of shape the logo should be. 
+        type: 'list',
+        message: 'What shape would you like the logo to have?',
+        choices: ['Circle', 'Triangle', 'Square'],
+        name: 'logoShape',
+    },
+    {
+        // Prompt the user what color the logo should from terminal.
+        type:'input',
+        message: 'Enter shape color value',
+        name: 'logoShapeColor',
+    },
+]
+
+
+function init(){
+inquirer.prompt(questionPrompts).then((answers) => {
+
+//Calls the shapes constructor
+const {Triangle, Circle, Shape } = require('./lib/shapes');
+
+//Creates the value of shape
+let shape;
+
+//Passes the shape color, logo color and logo text to the constructor. 
+//Switch statement to call a specfic function 
+switch (answers.logoShape) {
+case 'Circle':
+    shape = new Circle(answers.logoShapeColor,answers.logoTextColor, answers.logoText);
+    break;
+case 'Triangle':
+    shape = new Triangle(answers.logoShapeColor,answers.logoTextColor, answers.logoText);
+    break;
+case 'Square':
+    shape = new Shape(answers.logoShapeColor,answers.logoTextColor, answers.logoText);
+    break;
+default:
+    console.log('Invalid shape selection.');
+    return;
 }
+
+//Calls the render function from the shapes file
+const shapeTemp = shape.render();
+
+// Calls the write to file function to create the file. 
+writeToFile('./examples/newShape.svg' , generateShape(shapeTemp))
+});
+}
+
+// Shapes generateShapes function to generate the svg file.
+function generateShape(shape) {
+
+return `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+ ${shape}</svg>`;
+}
+
+//Write to file function to write the data to the file
+function writeToFile(fileName, data) {
+fs.writeFile(fileName, data, (err) =>
+/* Prints out error message if the file cannot be generate. 
+Prints out successful message if the file generates. */
+err ? console.error(err) : console.log('Shape generation is successful!')
+);
+}
+
+//Calls the init function to run the application.
+init();
